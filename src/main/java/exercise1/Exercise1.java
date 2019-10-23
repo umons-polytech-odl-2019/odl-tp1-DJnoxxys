e42626ae87400efd303e6c95e5efdd9703bbe786
Anthony <anthony.ZANOTTO@student.umons.ac.be>
2019-10-23 14:57:14 +0200

    exercice 1 ok

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,14 +1,19 @@
 package exercise1;
 
 public class Exercise1 {
+	// utiliser que par les tests unitaires
 	static Person createPerson(String name, int age) {
 		// Ajoutez les champs name et age à la classe Person.
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		//return null;
+		Person p = new Person (name , age);
+		return p;
 	}
 
 	public static void main(String[] args) {
+		Person p = new Person(args[0], Integer.parseInt(args[1]));
+		System.out.println(p.getName() + " " + p.getAge());
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,28 @@
 package exercise1;
+import java.util.*;
 
 class Person {
+	private String name;
+	private int age;
+
+	public Person (String name, int age) {
+		this.name = name;
+		this.age= age;
+	}
+
+	public String getName() {
+		return name;
+	}
+// set pour changer les elements
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 }
